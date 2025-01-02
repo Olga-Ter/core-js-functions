5631c6e36c7d765116492727268683bbf7f74573
Olga <c_h_e_r_r_y@tut.by>
2025-01-03 00:47:25 +0300

    feat: task getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -203,8 +203,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let current = startFrom - 1;
+  return () => {
+    current += 1;
+    return current;
+  };
 }
 
 module.exports = {
